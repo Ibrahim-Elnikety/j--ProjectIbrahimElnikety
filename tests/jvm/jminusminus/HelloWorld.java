7c4dc657d2b8d7af8e676a1c9b14680b5ad96949
Ibrahim-Elnikety <61748459+Ibrahim-Elnikety@users.noreply.github.com>
2026-06-13 09:52:37 -0700

    First commit part 1

---
+++ tests/jvm/jminusminus/HelloWorld.java
@@ -0,0 +1,13 @@
+// Copyright 2012- Bill Campbell, Swami Iyer and Bahar Akbal-Delibas
+//
+// Writes to standard output the message "Hello, World".
+package jminusminus;
+
+import java.lang.System;
+
+public class HelloWorld {
+    // Entry point.
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ tests/spim/jminusminus2/HelloWorld.java
@@ -0,0 +1,14 @@
+// Copyright 2012- Bill Campbell, Swami Iyer and Bahar Akbal-Delibas
+//
+// Writes the message "Hello, World" to standard output.
+package jminusminus2;
+
+import jminusminus.SPIM;
+
+public class HelloWorld {
+    // Entry point.
+    public static void main(String[] args) {
+        SPIM.printString("Hello, World");
+        SPIM.printString("\n");
+    }
+}
